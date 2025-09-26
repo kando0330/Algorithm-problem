a691ad0cc7cd6697d06ba3c6a20fdcd76a473282
DOHUN HAN <133339497+kando0330@users.noreply.github.com>
2025-09-26 17:50:14 +0900

    [level 2] Title: 올바른 괄호, Time: 16.14 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution {
+	boolean solution(String s) {
+
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (c == '(') {
+				stack.push(1);
+			} else if (c == ')') {
+				if (stack.isEmpty()) {
+					return false;
+				}
+				stack.pop();
+			}
+		}
+
+		if (stack.isEmpty()) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+}
